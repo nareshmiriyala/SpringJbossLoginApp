1fc215ea55b05508fbb52101b4b221f4e8ebb510
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-05-21 22:29:05 +1000

    First commit

---
+++ src/main/java/com.dellnaresh.spring.config/SpringWebAppInitializer.java
@@ -0,0 +1,17 @@
+package com.dellnaresh.spring.config;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringWebAppInitializer  extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { RootConfig.class };
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class };
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nareshm
+  Date: 21/05/2015
+  Time: 9:27 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h1>Welcome</h1>
+
+</body>
+</html>
